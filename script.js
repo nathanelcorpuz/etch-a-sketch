c3d6c62a7f5b0b105b068500870620f7ebcb6fc1
Nat Corpuz <nate.corpuz1@gmail.com>
2022-02-09 14:40:19 +0800

    grid basic logic complete

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
 //setup grid
 const container = document.querySelector('.container');
-let gridSize = 32;
-let gridItems = gridSize**2;
-container.style = `grid-template-rows: repeat(${gridSize}, 1fr);`;
-container.style = `grid-template-columns: repeat(${gridSize}, 1fr);`;
+let gridItemsList = [];
+let gridSize = 0;
+let gridItems = 0
 let gridItem;
-for (let i = 0; i < gridItems; i++) {
-    gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    gridItem.style = 'outline: 1px solid gray;';
-    container.appendChild(gridItem);
+
+promptUser();
+
+function promptUser() {
+    gridSize = prompt('Enter grid size #x# (4 to 100)');
+    if (gridSize > 100 || gridSize < 1) {
+        alert('Out of range, please try again');
+        promptUser();
+    }
+
+    else if (!gridSize) {
+        alert('Invalid input, please try again');
+        promptUser();
+    }
+    gridItems = gridSize**2;
+    createGrid();
 }
 
-function itemAdd(e) {
-    e.target.style = 'background: black';
+function createGrid() {
+    container.style = `grid-template-rows: repeat(${gridSize}, 1fr);`;
+    container.style = `grid-template-columns: repeat(${gridSize}, 1fr);`;
+    for (let i = 0; i < gridItems; i++) {
+        gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridItem.style = 'outline: 1px solid gray;';
+        container.appendChild(gridItem);
+    }
+    gridItemsList = document.querySelectorAll('div.grid-item');
 }
 
-const gridItemsList = document.querySelectorAll('div.grid-item');
-container.addEventListener('mousedown', () => {
-    gridItemsList.forEach(item => item.addEventListener('mouseenter', itemAdd));
-  })
 
-container.addEventListener('mouseup', () => {
-    gridItemsList.forEach(item => item.removeEventListener('mouseenter', itemAdd))
-})
+//grid coloring
+gridClick();
+function colorChange(e) {
+    e.target.style = 'background-color: black';
+}
+function gridClick() {
+    container.addEventListener('mousedown', updateGrid);
+    container.addEventListener('mouseup', removeColor);
+}
+function removeColor(e) {
+    e.target.style = 'background-color: black';
+    gridItemsList.forEach(item => item.removeEventListener('mouseenter', colorChange))
+}
+function updateGrid(e) {
+    e.target.style = 'background-color: black';
+    gridItemsList.forEach(item => item.addEventListener('mouseenter', colorChange));
+}
+
+//clear grid
+const clearBtn = document.querySelector('.clear');
+clearBtn.addEventListener('click', clearGrid);
+function clearGrid() {
+    gridItemsList.forEach(item => {
+        item.remove();
+    });
+    gridItemsList = [];
+    gridItems = 0;
+    gridSize = 0;
+    promptUser();
+}
