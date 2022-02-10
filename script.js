78a34ea98fa526eb823733a058b80b29bebde683
Nat Corpuz <nate.corpuz1@gmail.com>
2022-02-10 12:44:17 +0800

    add how-to, fix clear bug, add line toggle, add color picker

--- script.js
+++ script.js
@@ -5,21 +5,27 @@ let gridSize = 0;
 let gridItems = 0
 let gridItem;
 
-promptUser();
+// promptUser();
 
 function promptUser() {
     gridSize = prompt('Enter grid size #x# (4 to 100)');
     if (gridSize > 100 || gridSize < 1) {
+        gridSize = 0;
+        gridItems = 0;
         alert('Out of range, please try again');
         promptUser();
     }
 
     else if (!gridSize) {
+        gridSize = 0;
+        gridItems = 0;
         alert('Invalid input, please try again');
         promptUser();
     }
-    gridItems = gridSize**2;
-    createGrid();
+    else {
+        gridItems = gridSize**2;
+        createGrid();
+    }
 }
 
 function createGrid() {
@@ -28,35 +34,53 @@ function createGrid() {
     for (let i = 0; i < gridItems; i++) {
         gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
-        gridItem.style = 'outline: 1px solid gray;';
+        gridItem.classList.add('grid-lines-toggle');
         container.appendChild(gridItem);
     }
     gridItemsList = document.querySelectorAll('div.grid-item');
 }
 
 
+//toggle grid lines
+const gridToggleBtn = document.querySelector('.grid-toggle');
+gridToggleBtn.addEventListener('click',toggleGridLines);
+
+function toggleGridLines() {
+    gridItemsList.forEach(item => {
+        item.classList.toggle('grid-lines-toggle');
+    })
+}
+
+//color picker
+const colorPicker = document.querySelector('#color-picker');
+let gridColor = colorPicker.value;
+let defColor = 'background-color: white';
+
 //grid coloring
 gridClick();
 function colorChange(e) {
-    e.target.style = 'background-color: black';
+    e.target.style = `background-color: ${colorPicker.value}`;
 }
 function gridClick() {
     container.addEventListener('mousedown', updateGrid);
     container.addEventListener('mouseup', removeColor);
+    
 }
 function removeColor(e) {
-    e.target.style = 'background-color: black';
+    e.target.style = `background-color: ${colorPicker.value}`;
     gridItemsList.forEach(item => item.removeEventListener('mouseenter', colorChange))
 }
 function updateGrid(e) {
-    e.target.style = 'background-color: black';
+    e.target.style = `background-color: ${colorPicker.value}`;
     gridItemsList.forEach(item => item.addEventListener('mouseenter', colorChange));
 }
+
 
 //clear grid
 const clearBtn = document.querySelector('.clear');
 clearBtn.addEventListener('click', clearGrid);
 function clearGrid() {
+    gridItemsList = document.querySelectorAll('div.grid-item');
     gridItemsList.forEach(item => {
         item.remove();
     });
@@ -66,7 +90,5 @@ function clearGrid() {
     promptUser();
 }
 
-//add button for grid toggle
-//add button for color picker
-//add button for erasing grid
 //add feature for erasing a cell via click
+//add button for erase toggle
